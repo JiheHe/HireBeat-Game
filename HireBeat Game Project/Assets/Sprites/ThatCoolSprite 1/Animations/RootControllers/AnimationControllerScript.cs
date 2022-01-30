88363af83089f481c173994c8ba21caef5425cb0
JiheHe <nickhe2003@gmail.com>
2022-01-29 19:24:24 -0800

    FinishedRootController
    Now we just need someone to reptitvely expand the library..

--- HireBeat Game Project/Assets/Sprites/ThatCoolSprite 1/Animations/RootControllers/AnimationControllerScript.cs
+++ HireBeat Game Project/Assets/Sprites/ThatCoolSprite 1/Animations/RootControllers/AnimationControllerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tester : MonoBehaviour
+public class AnimationControllerScript : MonoBehaviour
 {
     Animator animator;
 
