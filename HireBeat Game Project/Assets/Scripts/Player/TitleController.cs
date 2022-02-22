f05e5e48ce5a177be7b4d6ed8dce593e554028c8
JiheHe <nickhe2003@gmail.com>
2022-02-22 14:03:54 -0500

    implemented RPC title controller
    Very easy! Since all objs share same title, just need to change index and the changes show up on all. Make sure to pass in local title index tho so players who joined later won't get a default titleIndex = 0 of other clients (from start, prob will change in the future). So in general, update local vars too!

--- HireBeat Game Project/Assets/Scripts/Player/TitleController.cs
+++ HireBeat Game Project/Assets/Scripts/Player/TitleController.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class TitleController : MonoBehaviour
 {
     int titleIndex;
+    PhotonView view;
+
     // Start is called before the first frame update
     void Start()
     {
         titleIndex = 0;
+        view = GetComponent<PhotonView>();
     }
 
     public void changeTitle(int nextIndex)
     {
+        view.RPC("changeTitleRPC", RpcTarget.AllBuffered, nextIndex, titleIndex); //need to let other clients know the current title index (might not be 0 if joined late)
+    }
+
+    [PunRPC]
+    void changeTitleRPC(int nextIndex, int titleIndex)
+    {
         transform.GetChild(titleIndex).gameObject.SetActive(false);
         transform.GetChild(nextIndex).gameObject.SetActive(true);
-        titleIndex = nextIndex;
+        this.titleIndex = nextIndex;
     }
 }
