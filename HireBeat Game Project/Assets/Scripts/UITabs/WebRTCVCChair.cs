879ce38fa09a01518184691ec69479734603688b
JiheHe <nickhe2003@gmail.com>
2022-04-21 04:21:54 -0400

    Trying to fix transform displacement bug
    fdas

--- HireBeat Game Project/Assets/Scripts/UITabs/WebRTCVCChair.cs
+++ HireBeat Game Project/Assets/Scripts/UITabs/WebRTCVCChair.cs
@@ -86,11 +86,29 @@ public class WebRTCVCChair : MonoBehaviour
             youThePlayer.GetComponent<playerController>().enabled = false;
             //youThePlayer.GetComponent<playerController>().ForceTurnTowards(userInChairTurnOrientation.x, userInChairTurnOrientation.y);
             youThePlayer.transform.position = GetComponent<Transform>().position -  userSitPositionOffset;
+            StartCoroutine(MakeCharUploadInPlaceMoving(20));
             Debug.Log("Joining Private VC");
             //Join chair
             terminal.AnnounceChairOccupation(chairId, true, terminal.myID);
             terminal.InitializeWebRTCCall();
             SetCurrentChairOwner();
+        }
+    }
+
+    IEnumerator MakeCharUploadInPlaceMoving(int numTimes)
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        if (numTimes <= 0)
+        {
+            yield return null;
+        }
+        else
+        {
+            youThePlayer.transform.position = Vector2.MoveTowards(youThePlayer.transform.position,
+                GetComponent<Transform>().position - userSitPositionOffset, 0);
+            yield return null;
+            StartCoroutine(MakeCharUploadInPlaceMoving(numTimes-1));
         }
     }
 
